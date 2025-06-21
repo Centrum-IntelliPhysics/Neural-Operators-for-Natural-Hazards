d0c7ff3f2d1dc70fa231f05f62f57ed16e682559
Somdatta Goswami <34455887+somdattagoswami@users.noreply.github.com>
2025-06-21 13:35:19 -0400

    Updated units

--- wind/computational_cost.m
+++ wind/computational_cost.m
@@ -1,12 +1,18 @@
+close all
+clear all
+clc
+
 % Simulation time comparison data
-offline_data_gen = 1200;  % Time to generate 800 samples (seconds)
+num_train_data = 800;
+traditional_sim = 472;      % Traditional simulation time (seconds)
+offline_data_gen = traditional_sim * num_train_data;  % Time to generate 800 samples (seconds)
 offline_training = 1500;   % Neural operator training time (seconds)
 online_inference = 0.01;    % Inference time per simulation (seconds)
-traditional_sim = 1.48;      % Traditional simulation time (seconds)
+
 
 % Create breakeven analysis plot
 figure;
-N = 0:5000;  % Number of simulations
+N = 0:2000;  % Number of simulations
 ml_total = (offline_data_gen + offline_training) + N*online_inference;
 traditional_total = N*traditional_sim;
 
@@ -23,7 +29,7 @@ grid on;
 
 % Find breakeven point and add annotation
 breakeven = floor((offline_data_gen + offline_training) / (traditional_sim - online_inference)) + 1; 
-annotation('textbox', [0.5,0.7,0.3,0.1],...
+annotation('textbox', [0.47,0.73,0.3,0.1],...
            'String', sprintf('Breakeven at %d simulations', breakeven),...
            'FitBoxToText', 'on',...
            'FontSize', 12);
